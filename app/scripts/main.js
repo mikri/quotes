ef55fa30715744ab7b1b3460d32076d052f1dc8d
Mikko Hyytiäinen <mikko.hyytiainen@gmail.com>
2015-12-17 07:22:27 +0200

    first commit with project files

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$("blockquote p").slabText({
+  // Don't slabtext the headers if the viewport is under 380px
+  "viewportBreakpoint":380
+});
+
+$( '#cbp-qtrotator' ).cbpQTRotator();
